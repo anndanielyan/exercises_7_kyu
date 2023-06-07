cf936f79484998fd8906140844b9eb4524aec59c
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:51:55 +0400

    exercise 6

--- auth.js
+++ auth.js
@@ -1,1 +1,22 @@
+function killer(suspects, deadPeople) {
+  for (let suspect in suspects) {
+    if (
+      deadPeople.every((deadP) => {
+        return suspects[suspect].includes(deadP);
+      })
+    )
+      return suspect;
+  }
+  return;
+}
 
+console.log(
+  killer(
+    {
+      James: ['Jacob', 'Bill', 'Lucas'],
+      Johnny: ['David', 'Kyle', 'Lucas'],
+      Peter: ['Lucy', 'Kyle'],
+    },
+    ['Lucas', 'Bill']
+  )
+);
