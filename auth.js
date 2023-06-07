adb13ffd696eec35e1d745438f394a853346a238
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:54:37 +0400

    exercise 8

--- auth.js
+++ auth.js
@@ -1,1 +1,12 @@
+function counter() {
+  let x = 0;
+  return function () {
+    x++;
+    return x;
+  };
+}
 
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
