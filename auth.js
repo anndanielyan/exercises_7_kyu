2081628d4379dea0093cb63d012f828e983f69f9
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:48:25 +0400

    exercise 4

--- auth.js
+++ auth.js
@@ -1,1 +1,15 @@
+function solve(s) {
+  const arr = s.split('');
+  const strL = arr.filter((item) => {
+    return item === item.toLowerCase();
+  });
+  const strU = arr.filter((item) => {
+    return item === item.toUpperCase();
+  });
 
+  if (strL.length === strU.length) {
+    return s.toLowerCase();
+  }
+  if (strU.length > strL.length) return s.toUpperCase();
+  return s.toLowerCase();
+}
