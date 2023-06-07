dba9387940c46bc81fddbb817115c84fdf363c30
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:39:47 +0400

    exercise 1

--- auth.js
+++ auth.js
@@ -1,1 +1,7 @@
+function consecutive(arr, a, b) {
+  if (arr.indexOf(a) === arr.indexOf(b) + 1) return true;
+  if (arr.indexOf(b) === arr.indexOf(a) + 1) return true;
+  return false;
+}
 
+console.log(consecutive([1, 3, 5, 7], 3, 1));
