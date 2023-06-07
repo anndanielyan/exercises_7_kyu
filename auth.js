6797a93532ee857698889cefdfa2754bf662cf14
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:43:20 +0400

    exercise 2

--- auth.js
+++ auth.js
@@ -1,1 +1,20 @@
+function separateTypes(input) {
+  const num = input.filter((i) => {
+    return typeof i === 'number';
+  });
 
+  const str = input.filter((i) => {
+    return typeof i === 'string';
+  });
+
+  const boolean = input.filter((i) => {
+    return typeof i === 'boolean';
+  });
+
+  const opt = {};
+
+  if (num.length > 0) opt.number = num;
+  if (str.length > 0) opt.string = str;
+  if (boolean.length > 0) opt.boolean = boolean;
+  return opt;
+}
