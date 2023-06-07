06b4fdded6cdeced6fa3fdea8ea0e6d8fe7c7c80
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:53:22 +0400

    exercise 7

--- auth.js
+++ auth.js
@@ -1,1 +1,23 @@
+function i(word) {
+  const arrOfVowels = ['a', 'e', 'i', 'o', 'u'];
+  if (word.startsWith('I') || word.charAt(0) === word.charAt(0).toLowerCase())
+    return 'Invalid word';
 
+  let numOfVowels = 0;
+  let numOfCons = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (arrOfVowels.includes(word.charAt(i).toLowerCase())) {
+      numOfVowels++;
+    } else {
+      numOfCons++;
+    }
+  }
+
+  if (numOfVowels >= numOfCons) {
+    return 'Invalid word';
+  }
+
+  return `i${word}`;
+}
+
+console.log(i('Peace'));
