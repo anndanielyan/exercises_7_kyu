fcbd8bef9dba81bab8775d3ab51ed46392fa2a38
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:56:49 +0400

    exercise 10

--- auth.js
+++ auth.js
@@ -1,1 +1,8 @@
+function secondSymbol(s, symbol) {
+  if (s.indexOf(symbol) === -1 || !s.includes(symbol)) return -1;
 
+  if (s.indexOf(symbol, s.indexOf(symbol) + 1) === -1) return -1;
+  return s.indexOf(symbol, s.indexOf(symbol) + 1);
+}
+
+console.log(secondSymbol('hello', 'l'));
