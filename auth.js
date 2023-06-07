bd18db12e6d9cb56ffb4ce8d4725155a7020aa70
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:46:34 +0400

    exercise 3

--- auth.js
+++ auth.js
@@ -1,1 +1,17 @@
-
+function lastDigit(n, d) {
+  if (d > n.toString().split('').length) {
+    return n
+      .toString()
+      .split('')
+      .map((item) => Number(item));
+  } else if (d <= 0) {
+    return [];
+  } else {
+    const arr = n.toString().split('');
+    const newArray = arr.slice(arr.length - d);
+    return newArray.map((item) => {
+      return Number(item);
+    });
+  }
+}
+console.log(lastDigit(1343, 5));
