06886f885571b5f35f8f699f74b8bfe6fc3e8385
Anna Danielyan <anna96danielyan@gmail.com>
2023-06-07 19:55:44 +0400

    exercise 9

--- auth.js
+++ auth.js
@@ -1,1 +1,12 @@
-
+function validParentheses(parenStr) {
+  const arr = [];
+  for (let i = 0; i < parenStr.length; i++) {
+    const el = parenStr[i];
+    if (el === '(') arr.push(el);
+    if (el === ')') {
+      if (arr.length === 0) return false;
+      arr.pop();
+    }
+  }
+  return arr.length === 0;
+}
